c75f6611158db5ffe7cad93bc6c8fe7c37301aad
Stephen Harman <35758825+StephenFunCode@users.noreply.github.com>
2023-07-14 10:18:20 -0600

    Added action to add new joke in the new module

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,3 +1,24 @@
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const name = form.get("name");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof content !== "string" || typeof name !== "string") {
+    throw new Error("Form not submitted correctly.");
+  }
+
+  const fields = { content, name };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
@@ -5,7 +26,7 @@ export default function NewJokeRoute() {
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Joke Name: <input type="text" name="name" />
           </label>
         </div>
         <div>
